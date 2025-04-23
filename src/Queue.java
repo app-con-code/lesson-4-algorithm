53d42b8a05ae8b45bb57e68eafb9bcaf38e8681d
goforit615 <bupark82@gsm.hs.kr>
2025-04-23 11:40:33 +0900

    initial commit

--- src/Queue.java
+++ src/Queue.java
@@ -1,6 +1,6 @@
 public class Queue {
     public static void main(String[] args) {
-        /* read the name of the user */
+        /* read the num of the user */
 
         /* print out "Output" followed by the input (see description) */
     }
